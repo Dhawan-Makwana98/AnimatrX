14e69b3e1616910345a8a920482b6b6325cb663a
Dhawan-Makwana98 <dhavanmak@gmail.com>
2024-08-12 17:46:49 +0530

    Minor Fixes

--- app/src/main/java/com/example/animatrx/MainActivity.java
+++ app/src/main/java/com/example/animatrx/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.animatrx;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,13 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private AnimationManager animationManager;
-    private Handler handler = new Handler();
+    private final Handler handler = new Handler();
     private Runnable animationRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.BLACK);
+        }
 
         imageView = findViewById(R.id.imageView);
         Button button = findViewById(R.id.button);
@@ -39,12 +53,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Change the image when the button is clicked
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animationManager.nextImage();
-                updateImage();
-            }
+        button.setOnClickListener(v -> {
+            animationManager.nextImage();
+            updateImage();
         });
     }
 
@@ -70,41 +81,35 @@ public class MainActivity extends AppCompatActivity {
                 .rotationBy(-360)
                 .setDuration(1000) // Duration for movement and rotation
                 .setInterpolator(new AccelerateDecelerateInterpolator()) // Smooth in and out
-                .withEndAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Zoom out and change the image
-                        imageView.animate()
-                                .scaleX(0f)
-                                .scaleY(0f)
-                                .setDuration(900) // Zoom-out duration
-                                .withEndAction(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        // Change the image after zooming out
-                                        animationManager.nextImage();
-                                        updateImage();
+                .withEndAction(() -> {
+                    // Zoom out and change the image
+                    imageView.animate()
+                            .scaleX(0f)
+                            .scaleY(0f)
+                            .setDuration(900) // Zoom-out duration
+                            .withEndAction(() -> {
+                                // Change the image after zooming out
+                                animationManager.nextImage();
+                                updateImage();
 
-                                        // Zoom in and return to the original position with a rotation
-                                        imageView.animate()
-                                                .x(animationManager.getOriginalX())
-                                                .y(animationManager.getOriginalY())
-                                                .scaleX(1f)
-                                                .scaleY(1f)
-                                                .rotationBy(360)
-                                                .setDuration(900) // Zoom-in and return duration
-                                                .setInterpolator(new AccelerateDecelerateInterpolator()) // Smooth in and out
-                                                .start();
-                                    }
-                                })
-                                .start();
-                    }
+                                // Zoom in and return to the original position with a rotation
+                                imageView.animate()
+                                        .x(animationManager.getOriginalX())
+                                        .y(animationManager.getOriginalY())
+                                        .scaleX(1f)
+                                        .scaleY(1f)
+                                        .rotationBy(360)
+                                        .setDuration(900) // Zoom-in and return duration
+                                        .setInterpolator(new AccelerateDecelerateInterpolator()) // Smooth in and out
+                                        .start();
+                            })
+                            .start();
                 })
                 .start();
     }
 
     private void updateImage() {
-        int resId = getResources().getIdentifier(animationManager.getCurrentImage(), "drawable", getPackageName());
+        @SuppressLint("DiscouragedApi") int resId = getResources().getIdentifier(animationManager.getCurrentImage(), "drawable", getPackageName());
         imageView.setImageResource(resId);
     }
 
@@ -120,16 +125,16 @@ public class MainActivity extends AppCompatActivity {
         private float yOffset = 0;
         private int imageId = 0;
 
-        private Handler timerHandler = new Handler();
+        private final Handler timerHandler = new Handler();
         private Runnable offsetRunnable;
 
-        private float maxX;
-        private float maxY;
-        private float originalX;
-        private float originalY;
-        private Random random = new Random();
+        private final float maxX;
+        private final float maxY;
+        private final float originalX;
+        private final float originalY;
+        private final Random random = new Random();
 
-        private String[] imageArray = {"diwali_fireworks", "colourfull_fan", "shared1", "shared2", "shared3", "shared4"};
+        private final String[] imageArray = {"diwali_fireworks", "colourfull_fan", "shared1", "shared2", "shared3", "shared4"};
 
         public AnimationManager(float maxX, float maxY) {
             this.maxX = maxX;
